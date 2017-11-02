346a339bc142c9dd24be954a7c1991de430771ca
Raphy998 <raphaelmoser@gmx.at>
2017-11-02 13:04:31 +0100

    updated trigger for entry
    check for an valid duedate added

--- SQL/tr_entry.sql
+++ SQL/tr_entry.sql
@@ -8,4 +8,9 @@ BEGIN
    IF NOT (:new.flag = 'R' OR :new.flag = 'O') THEN 
         RAISE_APPLICATION_ERROR(-20013, 'Entry flag is only O or R is allowed');
    END IF;
+   IF NOT (:new.duedate IS null) THEN
+        IF (trunc(:new.duedate) <= trunc(:new.postedOn)) THEN 
+            RAISE_APPLICATION_ERROR(-20014, 'No valid duedate');
+        END IF;
+    END IF;
 END;
