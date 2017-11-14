e1ee28c6e88948687196ccb02f99811979b8e429
Moe <raphaelmoser@gmx.at>
2017-11-14 09:22:03 +0100

    trigger to allow only 5 sessions per user

---
+++ SQL/tr_session.sql
@@ -0,0 +1,11 @@
+create or replace TRIGGER TR_SESSION 
+BEFORE INSERT ON TSESSION
+FOR EACH ROW
+DECLARE
+    v_count number;
+BEGIN
+    select count(*) into v_count from tsession where username = :new.username group by username;
+    IF ( v_count = 5) then
+        delete from tsession where username = :new.username and EXPIRYDATE = (select min(EXPIRYDATE) from TSESSION where username = :new.username);
+    end if;
+END;
