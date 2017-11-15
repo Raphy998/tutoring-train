ca918b1de8ee77f9e05570e13561ff4f9d6b25e7
Marco Wilscher <wilschem@edu.htl-villach.at>
2017-11-15 16:24:37 +0100

    Implement supported language enumeration (English, German)

---
+++ TutoringTrainAdminClient/src/main/java/at/tutoringtrain/adminclient/main/Language.java
@@ -0,0 +1,22 @@
+package at.tutoringtrain.adminclient.main;
+
+import java.util.Locale;
+
+/**
+ * Supported language enumeration
+ * @author Marco Wilscher marco.wilscher@edu.htl-villach.at
+ */
+public enum Language {
+    EN(Locale.ENGLISH), 
+    DE(Locale.GERMAN);
+    
+    private final Locale locale;
+
+    private Language(Locale locale) {
+        this.locale = locale;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+}
