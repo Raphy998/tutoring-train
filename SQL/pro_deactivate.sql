6fdfed3f75733aeeb3db95edbdae0864f75bb248
santnerelias <elias.santner@edu.htl-villach.at>
2017-11-17 07:59:42 +0100

    Update pro_deactivate.sql

--- SQL/pro_deactivate.sql
+++ SQL/pro_deactivate.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE PROCEDURE PRO_DEACTIVATE AS 
+create or replace PROCEDURE PRO_DEACTIVATE AS 
 BEGIN
   Update entry set isactive = '0' where duedate <= sysdate;
+  commit;
 END PRO_DEACTIVATE;
