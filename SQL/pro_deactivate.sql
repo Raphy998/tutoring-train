369edbc2d0e273fdd3cac6aa438d36cb3a90f86f
santnerelias <elias.santner@edu.htl-villach.at>
2017-11-17 07:58:15 +0100

    Simplified

--- SQL/pro_deactivate.sql
+++ SQL/pro_deactivate.sql
@@ -1,10 +1,4 @@
 CREATE OR REPLACE PROCEDURE PRO_DEACTIVATE AS 
 BEGIN
-  FOR entry_rec IN (
-        SELECT id
-          FROM ENTRY
-         WHERE DUEDATE <= sysdate)
-   LOOP
-   UPDATE entry SET ISACTIVE = '0' WHERE ID = entry_rec.id;
-   END LOOP;
+  Update entry set isactive = '0' where duedate <= sysdate;
 END PRO_DEACTIVATE;
