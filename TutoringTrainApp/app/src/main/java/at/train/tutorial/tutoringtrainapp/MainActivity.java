22d7b11187bc87235aeae5c92fdd418a88ebe142
Moe <raphaelmoser@gmx.at>
2017-11-17 09:17:09 +0100

    adjust the recyclerview in mainactivity

--- TutoringTrainApp/app/src/main/java/at/train/tutorial/tutoringtrainapp/MainActivity.java
+++ TutoringTrainApp/app/src/main/java/at/train/tutorial/tutoringtrainapp/MainActivity.java
@@ -1,21 +1,12 @@
 package at.train.tutorial.tutoringtrainapp;
 
-import android.content.Context;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
@@ -40,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         layoutManager = new LinearLayoutManager(this);
         recView.setLayoutManager(layoutManager);
-        adapter = new PlanetAdapter(this.planetList,getApplicationContext());
+        adapter = new CustomAdapter(this.planetList,getApplicationContext());
 
         planetList.add("foo");
         planetList.add("bar");
@@ -72,45 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder>{
 
-        private LayoutInflater inflater;
-        private Context context;
-        private ArrayList<String> test;
-
-        public CustomAdapter(Context context,ArrayList<String> test) {
-            inflater = LayoutInflater.from(context);
-            this.context = context;
-            this.test=test;
-        }
-
-        @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = inflater.inflate(R.layout.planet_row, parent, false);
-            MyViewHolder holder = new MyViewHolder(view);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            holder.serial_number.setText(test.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return test.size();
-        }
-
-        class MyViewHolder extends RecyclerView.ViewHolder
-        {
-            TextView serial_number;
-
-            public MyViewHolder(View itemView) {
-                super(itemView);
-                serial_number = (TextView)itemView.findViewById(R.id.text_id);
-            }
-        }
-    }
 
 
 
