5c0dde2dccc33dcb830ebd731d033ae5767a443c
Marco Wilscher <wilschem@edu.htl-villach.at>
2017-12-09 00:24:38 +0100

    Add offer/request classes

---
+++ TutoringTrainAdminClient/src/main/java/at/tutoringtrain/adminclient/data/Entry.java
@@ -0,0 +1,127 @@
+package at.tutoringtrain.adminclient.data;
+
+import at.tutoringtrain.adminclient.datamapper.DataMappingViews;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 
+ * @author Marco Wilscher marco.wilscher@edu.htl-villach.at
+ */
+public abstract class Entry {
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class
+    })
+    private BigDecimal id;
+    
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class,
+        DataMappingViews.Entry.Out.Create.class
+    })
+    private String headline;
+    
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class,
+        DataMappingViews.Entry.Out.Create.class
+    })
+    private String description;
+    
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class
+    })
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+    private Date postedon;
+    
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class
+    })
+    private Character isactive;
+       
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class,
+        DataMappingViews.Entry.Out.Create.class
+    })
+    private Subject subject;
+    
+    @JsonView({
+        DataMappingViews.Entry.In.Get.class,
+        DataMappingViews.Entry.In.Create.class
+    })
+    private User user;
+    
+    public Entry() {
+    }
+
+    public Entry(BigDecimal id) {
+        this.id = id;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getPostedon() {
+        return postedon;
+    }
+
+    public void setPostedon(Date postedon) {
+        this.postedon = postedon;
+    }
+
+    public Character getIsactive() {
+        return isactive;
+    }
+
+    public void setIsactive(Character isactive) {
+        this.isactive = isactive;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry {" + "id=" + id + ", headline=" + headline + ", description=" + description + ", postedon=" + postedon + ", isactive=" + isactive + ", subject=" + subject + ", user=" + user + '}';
+    }    
+}
--- TutoringTrainAdminClient/src/main/java/at/tutoringtrain/adminclient/data/Request.java
+++ TutoringTrainAdminClient/src/main/java/at/tutoringtrain/adminclient/data/Request.java
@@ -4,6 +4,6 @@ package at.tutoringtrain.adminclient.data;
  *
  * @author Marco Wilscher marco.wilscher@edu.htl-villach.at
  */
-public class Offer extends Entry {
+public class Request extends Entry {
 
 }
