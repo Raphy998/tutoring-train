29661e4ec4bb3d25d31e79a92bd1c7ec9b5ff746
moe <raphaelmoser@gmx.at>
2018-01-26 12:46:44 +0100

    revert merge

--- TutoringTrainAdminClient/src/main/java/at/tutoringtrain/adminclient/ui/listener/ApplicationExitListener.java
+++
@@ -1,9 +0,0 @@
-package at.tutoringtrain.adminclient.ui.listener;
-
-/**
- *
- * @author Marco Wilscher marco.wilscher@edu.htl-villach.at
- */
-public interface ApplicationExitListener {
-    void applicationExit();
-}
--- TutoringTrainAdminClient/src/main/java/at/tutoringtrain/adminclient/ui/search/SearchCategory.java
+++
@@ -1,9 +0,0 @@
-package at.tutoringtrain.adminclient.ui.search;
-
-/**
- *
- * @author Marco Wilscher marco.wilscher@edu.htl-villach.at
- */
-public enum SearchCategory {
-    ALL, USER, SUBJECT, OFFER
-}
