c6b52489696a3a06559f25472cb9c53a5ba615e9
santnerelias <elias.santner@edu.htl-villach.at>
2018-02-06 09:59:56 +0100

    fixed trigger

--- SQL/tr_session.sql
+++ SQL/tr_session.sql
@@ -4,7 +4,7 @@ FOR EACH ROW
 DECLARE
     v_count number;
 BEGIN
-    select count(*) into v_count from tsession where username = :new.username group by username;
+    select count(*) into v_count from tsession where username = :new.username;
     IF ( v_count = 5) then
         delete from tsession where username = :new.username and EXPIRYDATE = (select min(EXPIRYDATE) from TSESSION where username = :new.username);
     end if;
